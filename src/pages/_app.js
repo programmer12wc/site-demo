cd965b82e3cd52b3fadd0aa1eb77a9926a88616c
programmer12wc <150346262+programmer12wc@users.noreply.github.com>
2024-03-08 13:17:01 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,10 +2,14 @@ import "@/styles/globals.css";
 import "react-loading-skeleton/dist/skeleton.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import { CartProvider } from "react-use-cart";
+import Head from "next/head";
 import "swiper/css";
 
 export default function App({ Component, pageProps }) {
   return (
+    <Head>
+    <meta name="google-site-verification" content="7vBcpD4vU43j0H-JmisE6HiwfngtaXBTv7rz5maRciQ" />
+    </Head>
     <div className="app-container">
       <CartProvider>
         <Component {...pageProps} />
